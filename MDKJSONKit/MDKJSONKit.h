20e9e577a342251700b4628a4f73412607aab421
Julien Lacroix <julien.lacroix@soprasteria.com>
2016-02-02 15:07:16 +0100

    Initial commit

---
+++ MDKJSONKit/MDKJSONKit.h
@@ -0,0 +1,21 @@
+//
+//  MDKJSONKit.h
+//  MDKJSONKit
+//
+//  Created by Lagarde Quentin on 02/02/2016.
+//  Copyright © 2016 Sopra Steria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDKJSONKit.
+FOUNDATION_EXPORT double MDKJSONKitVersionNumber;
+
+//! Project version string for MDKJSONKit.
+FOUNDATION_EXPORT const unsigned char MDKJSONKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDKJSONKit/PublicHeader.h>
+
+#import <MDKJSONKit/JSONKit.h>
+
+
